c07d9020c3090a06086bc495703534cc166e272f
Aluno do Inatel <aluno@local.inatel.br>
2022-08-02 11:31:53 -0300

    Exercicio

---
+++ Primeira Aula1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int idade = 18;
+        int cpf = 299865520;
+        String nome = "Felipe Inacio";
+        String titulo = "sim";
+        System.out.println(idade);
+        System.out.println(nome);
+        if(titulo=="sim")
+        {
+            System.out.println("É capaz de votar");
+        }
+        else
+        {
+            System.out.println("Não é capaz de votar");
+        }
+    }
+}
